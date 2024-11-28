abe5efd3713c71ee4a922c2bb96e070a9acf094e
Leonardo Cabrera <lcnicolau@gmx.com>
2024-11-28 18:02:54 -0300

    This is where it all begins...

---
+++ README.md
@@ -0,0 +1,3 @@
+This is [CS50](https://cs50.harvard.edu/x/ "CS50’s Introduction to Computer Science") Final Project
+===================================================================================================
+> Just another to-do list app... but having fun with [htmx](https://htmx.org/ "High power tools for HTML")
---
+++ src/main/java/io/github/lcnicolau/cs50/todolist/Application.java
@@ -0,0 +1,13 @@
+package io.github.lcnicolau.cs50.todolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
