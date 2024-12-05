58be50cd732eb3c13410c594d57f4480c48ff1b4
Leonardo Cabrera <lcnicolau@gmx.com>
2024-12-05 10:43:54 -0300

    feat: enable csrf protection

--- src/main/java/io/github/lcnicolau/cs50/todolist/config/SecurityConfig.java
+++ src/main/java/io/github/lcnicolau/cs50/todolist/config/SecurityConfig.java
@@ -9,7 +9,6 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -64,7 +63,7 @@ class SecurityConfig {
                         .anyRequest().authenticated()
                 )
                 .cors(Customizer.withDefaults())
-                .csrf(AbstractHttpConfigurer::disable) // TODO: enable csrf
+                .csrf(Customizer.withDefaults())
                 .build();
     }
 
